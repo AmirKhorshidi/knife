860222af253b5b203abd6ac5ecfcaf54c9a8ab0b
AmirKhorshidi <92648158+AmirKhorshidi@users.noreply.github.com>
2026-05-07 21:54:30 +0330

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1778177813,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/amir-sheet0.png",
+		"images/dialog-sheet0.png",
+		"images/lives-sheet0.png",
+		"images/bad-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/good-sheet0.png",
+		"images/knife-sheet0.png",
+		"images/start-sheet0.png",
+		"images/line-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
